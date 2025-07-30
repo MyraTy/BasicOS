f745082ff8949acb15ed5ba771d77839c47f8303
MyraTy <tosinabaca@outlook.es>
2025-07-30 13:22:35 +0200

    Updated messages for debugging

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -31,13 +31,11 @@ mov cl, 0x02 ; and sector 2
 mov bx, GDT_LOCATION ; and dump the data into address 0xE00.
 int 0x13
 
-jc disk_crash_cf ; Carry flag set to 1 after disk read means there's an error
+jnc loadk ; Carry flag set to 1 after disk read means there's an error. If no errors, continue
 
-cmp dh, al
-jne disk_crash_nsectors ; There's also an error if the number of sectors to read changes after disk read
-
-jmp init_protected_mode ; If no errors occur while reading disk, continue
-
+mov bx, disk_crash_cf_msg_gdt
+call print_16
+jmp exit ; If an error occurs, hang
 
 loadk:
 mov ah, 2 ; Mandatory
@@ -50,20 +48,20 @@ mov bx, KERNEL_LOCATION ; and dump the data into address 0x1000.
 int 0x13
 
 
-jc disk_crash_cf ; Carry flag set to 1 after disk read means there's an error
+jc disk_crash_cf_kernel ; Carry flag set to 1 after disk read means there's an error
 
 cmp dh, al
-jne disk_crash_nsectors ; There's also an error if the number of sectors to read changes after disk read
+jne disk_crash_nsectors_kernel ; There's also an error if the number of sectors to read changes after disk read
 
 jmp init_protected_mode ; If no errors occur while reading disk, continue
 
-disk_crash_cf:
-    mov bx, disk_crash_cf_msg
+disk_crash_cf_kernel:
+    mov bx, disk_crash_cf_msg_kernel
     call print_16
     jmp exit
 
-disk_crash_nsectors:
-    mov bx, disk_crash_nsectors_msg
+disk_crash_nsectors_kernel:
+    mov bx, disk_crash_nsectors_msg_kernel
     call print_16
     jmp exit
 
@@ -105,16 +103,19 @@ exit:
 %include "./src/bootloader/bootprint.asm"
 
 initmsg:
-    db "Bootloader started without issues. Loading kernel...", 13, 10, 0
+    db "Bootloader started without issues. Retrieving kernel and GDT...", 13, 10, 0
 
-disk_crash_cf_msg:
-    db "Fatal: Something went wrong when reading disk: Carry flag is set.", 13, 10, 0
+disk_crash_cf_msg_gdt:
+    db "Fatal: Carry flag set when retrieving the GDT from disk.", 13, 10, 0
 
-disk_crash_nsectors_msg:
-    db "Fatal: Something went wrong when reading disk: Try highening the number of loaded sectors.", 13, 10, 0
+disk_crash_cf_msg_kernel:
+    db "Fatal: Carry flag set when retrieving the kernel from disk.", 13, 10, 0
+
+disk_crash_nsectors_msg_kernel:
+    db "Fatal: Number of sectors to read too low when retrieving the kernel from disk.", 13, 10, 0
 
 init32msg:
-    db "Now in 32-bit mode. Loading kernel...\n\r", 0
+    db "Now in 32-bit mode. Loading kernel...", 13, 10, 0
 
 times 510-($-$$) db 0 ; Pad up to 510 bytes         
 dw 0xaa55 ; Last two bytes are the bootloader sign
